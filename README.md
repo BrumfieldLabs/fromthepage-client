c93acf1fa93387b339ed84d7057e02f825adf25c
saracarl <saracarl@gmail.com>
2021-02-03 11:24:23 -0600

    bulk export client script

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # fromthepage-client
-clients for interacting with the FromThePage APIs
+Clients for interacting with the FromThePage APIs
+
+# Usage
+Download the client and run it from the command line.  It requires ruby and curl.  We tested with ruby 2.6.0, but most should work.
+
+Run it with your [API key](https://content.fromthepage.com/project-owner-documentation/api-keys/), your user slug, and an existing backup directory.  Your user slug is the part of the FromThePage URL for any of your projects, i.e. 
+`benwbrum` in the following URL `https://fromthepage.com/benwbrum/jeremiah-white-graves-diaries`.
+
+`ruby full_export.rb KEY SLUG OUTPUT_DIRECTORY`
+
+i.e.
+`ruby full_export.rb 2K59hIdQhKfDDQoBMvnXiAtt benwbrum backups`
+
+# Sponsorship
+The team is grateful to the Beinecke Library at Yale University for support of this feature.
+
+
---
+++ v1/full_export.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'pry'
+
+date=Time.now.strftime("%Y%m%d")
+key=ARGV[0]
+user=ARGV[1]
+download_path=ARGV[2]
+
+response = `curl -s -H 'Authorization: Token token=#{key}' 'http://localhost:3000/iiif/collections/#{user}'`
+collections = JSON.parse(response)
+collections["collections"].each do |collection_block|
+  collection = collection_block["@id"]
+  slug=collection.split('/').last
+  raw_response = `curl -s -H 'Authorization: Token token=#{key}' -X POST "http://localhost:3000/api/v1/bulk_export/#{slug}?plaintext_verbatim_page=true&plaintext_verbatim_work=true"`
+  response = JSON.parse(raw_response)
+  status_uri=response["status_uri"]
+  download_uri=response["download_uri"]
+  100.times.each do
+    sleep 10
+    status_response= `curl -s -H 'Authorization: Token token=#{key}' '#{status_uri}'`
+    status = JSON.parse(status_response)["status"]
+    if status == "finished"
+      filename = File.join(download_path, "#{date}-#{slug}.zip")
+      `curl -s -H 'Authorization: Token token=#{key}' '#{download_uri}' --output '#{filename}'`
+      puts "downloaded #{filename}"
+      break
+    end
+  end
+end
